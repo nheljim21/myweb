59cb2d0c75b820210e538b03919571a57876da74
Nheljim Edis <edisnheljim21@gmail.com>
2025-09-14 12:52:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+const updateDateEl = document.getElementById("update-date");
+if (updateDateEl) {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  updateDateEl.textContent = new Date().toLocaleDateString(undefined, options);
+}
